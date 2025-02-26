ff0377552b8f3708db4e57aed7b9fc009a7adad2
Dima <dm.razin@yandex.ru>
2025-02-26 18:29:16 +0300

    Создание анимации героя, настройка камеры
    Создал анимацию для бега, прыжка.
    Добавил в скрипт Hero настройку анимации.
    Создал скрипт CameraController, чтобы камера передвигалась за персонажем

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform Hero;
+    private Vector3 coordinate;
+
+    private void Awake()
+    {
+        if (!Hero)
+        {
+            Hero = FindObjectOfType<Hero>().transform;
+        }
+    }
+    private void Update()
+    {
+        coordinate = Hero.position;
+        coordinate.z = -10f;
+        transform.position = Vector3.Lerp(transform.position, coordinate, Time.deltaTime);
+    }
+}
--- Scripts/Hero.cs
+++ Scripts/Hero.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using static UnityEditor.VersionControl.Asset;
 
@@ -6,21 +7,35 @@ public class Hero : MonoBehaviour
     [SerializeField] private float speed = 3f;
     [SerializeField] private int lives = 3;
     [SerializeField] private float jumpForce = 10f;
+    private bool isGrounded = false;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator animator;
 
+    private States State
+    {
+        get { return (States)animator.GetInteger("state"); }
+        set { animator.SetInteger("state", (int)value); }
+    }
     private void Awake()
     {
         sprite = GetComponentInChildren<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Run()
     {
+        if (isGrounded) State = States.Run;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
         sprite.flipX = dir.x < 0.0f;
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
     void Start()
     {
@@ -30,9 +45,30 @@ public class Hero : MonoBehaviour
     
     void Update()
     {
+        if (isGrounded) State = States.Idle;
+
         if (Input.GetButton("Horizontal"))
         {
             Run();
         }
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        CheckGround();
     }
+
+    private void CheckGround()
+    {
+        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 0.3f);
+        isGrounded = collider.Length > 1;
+        if (!isGrounded) State = States.Jump;
+    }
+}
+
+public enum States
+{
+    Idle,
+    Run,
+    Jump
 }
